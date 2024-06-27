abe34dc189acf76e6f0143877bf51430049d6bbb
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-06-27 13:38:43 +0200

    refactor command functions into separate module

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+
+pub mod command {
+    use std::io;
+
+    pub fn add() -> Result<(), io::Error> { todo!() }
+
+    pub fn cat_file() -> Result<(), io::Error> { todo!() }
+
+    pub fn check_ignore() -> Result<(), io::Error> { todo!() }
+
+    pub fn checkout() -> Result<(), io::Error> { todo!() }
+
+    pub fn commit() -> Result<(), io::Error> { todo!() }
+
+    pub fn hash_object() -> Result<(), io::Error> { todo!() }
+
+    pub fn init() -> Result<(), io::Error> { todo!() }
+
+    pub fn log() -> Result<(), io::Error> { todo!() }
+
+    pub fn ls_files() -> Result<(), io::Error> { todo!() }
+
+    pub fn ls_tree() -> Result<(), io::Error> { todo!() }
+
+    pub fn rev_parse() -> Result<(), io::Error> { todo!() }
+
+    pub fn rm() -> Result<(), io::Error> { todo!() }
+
+    pub fn show_ref() -> Result<(), io::Error> { todo!() }
+
+    pub fn status() -> Result<(), io::Error> { todo!() }
+
+    pub fn tag() -> Result<(), io::Error> { todo!() }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io;
+use wyag::command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,82 +8,23 @@ fn main() {
     }
 
     let command = &args[1];
-    let result = match &command[..] {
-        "add" =>            { cmd_add() },
-        "cat-file" =>       { cmd_cat_file() },
-        "check-ignore" =>   { cmd_check_ignore() },
-        "checkout" =>       { cmd_checkout() },
-        "commit" =>         { cmd_commit() },
-        "hash-object" =>    { cmd_hash_object() },
-        "init" =>           { cmd_init() },
-        "log" =>            { cmd_log() },
-        "ls-files" =>       { cmd_ls_files() },
-        "ls-tree" =>        { cmd_ls_tree() },
-        "rev-parse" =>      { cmd_rev_parse() },
-        "rm" =>             { cmd_rm() },
-        "show-ref" =>       { cmd_show_ref() },
-        "status" =>         { cmd_status() },
-        "tag" =>            { cmd_tag() },
-        other => { panic!("{} is not a valid git command!", other)}
+    let _ = match &command[..] {
+        "add" =>            { command::add() },
+        "cat-file" =>       { command::cat_file() },
+        "check-ignore" =>   { command::check_ignore() },
+        "checkout" =>       { command::checkout() },
+        "commit" =>         { command::commit() },
+        "hash-object" =>    { command::hash_object() },
+        "init" =>           { command::init() },
+        "log" =>            { command::log() },
+        "ls-files" =>       { command::ls_files() },
+        "ls-tree" =>        { command::ls_tree() },
+        "rev-parse" =>      { command::rev_parse() },
+        "rm" =>             { command::rm() },
+        "show-ref" =>       { command::show_ref() },
+        "status" =>         { command::status() },
+        "tag" =>            { command::tag() },
+
+        other =>      { panic!("{} is not a valid git command!", other)}
     };
-}
-
-fn cmd_add() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_cat_file() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_check_ignore() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_checkout() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_commit() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_hash_object() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_init() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_log() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_ls_files() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_ls_tree() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_rev_parse() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_rm() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_show_ref() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_status() -> Result<(), io::Error> {
-    todo!()
-}
-
-fn cmd_tag() -> Result<(), io::Error> {
-    todo!()
 }
