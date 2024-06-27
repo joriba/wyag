2f58f8a20c5d6f632f333d24735124558c7bd359
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-06-27 13:31:27 +0200

    add functions for different git commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+use std::env;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("No command specified!");
+    }
+
+    let command = &args[1];
+    let result = match &command[..] {
+        "add" =>            { cmd_add() },
+        "cat-file" =>       { cmd_cat_file() },
+        "check-ignore" =>   { cmd_check_ignore() },
+        "checkout" =>       { cmd_checkout() },
+        "commit" =>         { cmd_commit() },
+        "hash-object" =>    { cmd_hash_object() },
+        "init" =>           { cmd_init() },
+        "log" =>            { cmd_log() },
+        "ls-files" =>       { cmd_ls_files() },
+        "ls-tree" =>        { cmd_ls_tree() },
+        "rev-parse" =>      { cmd_rev_parse() },
+        "rm" =>             { cmd_rm() },
+        "show-ref" =>       { cmd_show_ref() },
+        "status" =>         { cmd_status() },
+        "tag" =>            { cmd_tag() },
+        other => { panic!("{} is not a valid git command!", other)}
+    };
+}
+
+fn cmd_add() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_cat_file() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_check_ignore() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_checkout() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_commit() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_hash_object() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_init() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_log() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_ls_files() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_ls_tree() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_rev_parse() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_rm() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_show_ref() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_status() -> Result<(), io::Error> {
+    todo!()
+}
+
+fn cmd_tag() -> Result<(), io::Error> {
+    todo!()
 }
